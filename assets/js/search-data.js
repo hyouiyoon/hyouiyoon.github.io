6d70b54d45870a37171a1164d2ddf707f2060f56
hyouiyoon <hyouiyoon@ewha.ac.kr>
2025-12-11 13:11:04 +0000

    Deploying to gh-pages from @ hyouiyoon/hyouiyoon.github.io@802caac5febaa0e561b41d6b07690182756bc03c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,8 +416,8 @@ ninja.data = [{
           description: "2025.02.24Ewha W. Univ., Seoul, Korea",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2025/2025_1/";
-            },},{id: "projects-ebrl-homecoming",
-          title: 'EBRL Homecoming',
+            },},{id: "projects-biorobotics-lab-homecoming",
+          title: 'BioRobotics Lab. Homecoming',
           description: "2025.11.14Ewha W. Univ., Seoul, Korea",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2025/2025_2/";
@@ -436,11 +436,6 @@ ninja.data = [{
           description: "2025.12.10-13KSME, Gangwon-state, Korea",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2025/2025_5/";
-            },},{id: "projects-biorobotics-lab-homecoming",
-          title: 'BioRobotics Lab. Homecoming',
-          description: "2025.11.14Ewha W. Univ., Seoul, Korea",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2025/2025_6/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
